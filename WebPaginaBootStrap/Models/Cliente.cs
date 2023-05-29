abcfeb38e053511483584f29be36094e2ea3380d
Charles Magalhaes <charlesrm29@gmail.com>
2023-05-29 20:53:01 -0300

    Meu primeiro commit

---
+++ WebPaginaBootStrap/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebPaginaBootStrap.Models
+{
+    public partial class Cliente
+    {
+
+        public int Clienteid { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public int Id_cidade { get; set; }
+ 
+    }
+}
